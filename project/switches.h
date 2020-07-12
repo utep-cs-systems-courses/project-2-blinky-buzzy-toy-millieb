b8f33cbd9bd606a94a43036edd6c0b1f2e7a8878
millieb <mildred.brito@outlook.com>
2020-07-12 16:13:16 -0600

    added 4 buttons for the 4 extra switches on switches.h

--- project/switches.h
+++ project/switches.h
@@ -1,12 +1,22 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT3		/* switch1 is p1.3 */
-#define SWITCHES SW1		/* only 1 switch on this board */
+#define SW1 BIT0                /* switch1 is p1.3 */
+#define SW2 BIT1
+#define SW3 BIT2
+#define SW4 BIT3
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)  /* only 1 switch on this board */
 
 void switch_init();
 void switch_interrupt_handler();
 
-extern char switch_state_down, switch_state_changed; /* effectively boolean */
+/*Added state_down per switch*/
+extern char switch_state_down,
+  switch_state_down2,
+  switch_state_down3,
+  switch_state_down4,
+  switch_state_changed; /* effectively boolean */
+
+extern char state;
 
 #endif // included
