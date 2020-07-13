b7f9c9c86ea7dcd3d08cdd5ab82994d630b3e054
millieb <mildred.brito@outlook.com>
2020-07-12 18:14:47 -0600

    added toggle(), and led_dim()

--- project/led.c
+++ project/led.c
@@ -1,27 +1,67 @@
 #include <msp430.h>
 #include "led.h"
-
-unsigned char red_on = 0, green_on = 0;
-unsigned char led_changed = 0;
-
-static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
-
+#include "switches.h"
 
 void led_init()
 {
   P1DIR |= LEDS;		// bits attached to leds are output
-  led_changed = 1;
-  led_update();
+  switch_state_changed = 1;
 }
 
 void led_update()
 {
-  if (led_changed) {
-    char ledFlags = redVal[red_on] | greenVal[green_on];
-    
-    P1OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
-    P1OUT |= ledFlags;		     // set bit for on leds
-    led_changed = 0;
+  if (switch_state_changed)
+    {
+      char ledFlags = 0; //starting with leds off
+
+      ledFlags |= switch_state_down ? LED_GREEN : 0;
+      ledFlags |= switch_state_down ? 0 : LED_RED;
+
+      P1OUT &= (0xff - LEDS) | ledFlags; // clear bit for off leds
+      P1OUT |= ledFlags;		     // set bit for on leds
   }
+  switch_state_changed = 0;
 }
+
+void toggle_on()
+{
+  P1OUT = LED_RED;
+  __delay_cycles(500000);
+  
+  P1OUT = LED_GREEN;
+  __delay_cycles(500000);
+}
+
+void toggle_off()
+{
+  P1OUT = !LED_RED;
+  __delay_cycles(500000);
+  
+  P1OUT = !(LED_GREEN);
+  __delay_cycles(500000);
+}
+
+void led_dim()
+{
+  for(int i = 0; i < 1000; i++)
+    {
+      P1OUT = LED_RED;
+      __delay_cycles(100000);
+      
+      P1OUT = LED_GREEN;
+      __delay_cycles(100000);
+      
+      P1OUT = !LED_RED;
+      __delay_cycles(100000);
+      
+      P1OUT = !LED_GREEN;
+      __delay_cycles(100000);
+    }
+}
+
+
+
+
+
+
 
