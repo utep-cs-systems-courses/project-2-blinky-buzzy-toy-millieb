f32c524240e3d230cb4cb8b9e2eed27f778ede55
millieb <mildred.brito@outlook.com>
2020-07-12 18:44:22 -0600

    added stateMachines.c

---
+++ project/stateMachines.c
@@ -0,0 +1,34 @@
+#include <msp430.h>
+#include "stateMachines.h"
+#include "led.h"
+#include "buzzer.h"
+#include "switches.h"
+
+void state_advance(int state)
+{
+  switch(state)
+    {
+    case 1:
+      led_dim();
+      state = 0;
+      break;
+
+    case 2:
+      song();
+      state = 0;
+      break;
+
+    case 3:
+      siren_song();
+      state = 0;
+      break;
+
+    case 4:
+      led_siren();
+      state = 0;
+      break;
+
+    default:
+      break;
+    }
+}
